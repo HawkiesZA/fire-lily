201182b7c817b0c3552b6b97485845cac0624135
Gerrit Vermeulen <gerrit.vermeulen@assetpool.com>
2023-11-12 12:22:27 +0200

    Add contact page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,12 @@
 <script>
 </script>
 
-    <div class="container">
-        <div class="content">
-            <h2>Fire Lily Dance</h2>
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/lnbbpqs_8aI?si=iIDn8zC3r4W4Jroh" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-        </div>
+<div class="container">
+    <div class="content">
+        <h2>Fire Lily Dance</h2>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/lnbbpqs_8aI?si=iIDn8zC3r4W4Jroh" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
     </div>
+</div>
 
 <style>
     .container {
---
+++ src/routes/contact/+page.svelte
@@ -0,0 +1,49 @@
+<div class="container">
+    <div class="content">
+        <form method="POST">
+            <label>
+                Email
+                <input name="email" type="email">
+            </label>
+            <label>
+                Message
+                <textarea name="message" type="text"></textarea>
+            </label>
+            <button>Send</button>
+        </form>
+    </div>
+</div>
+
+<style>
+    .container {
+        padding: 2px 16px;
+    }
+    .content {
+        width: 100%;
+        padding: 10px;
+    }
+    @media only screen and (min-width: 768px) {
+        .content {
+            margin: auto;
+            width: 50%;
+            padding: 10px;
+        }
+    }
+
+    input[type=email], textarea {
+        display: inline-block;
+        width: 100%;
+        padding: 12px;
+        border: 1px solid #ccc;
+        border-radius: 4px;
+        box-sizing: border-box;
+        margin-top: 6px;
+        margin-bottom: 16px;
+        resize: vertical;
+    }
+
+    button {
+        float: right;
+        margin-bottom: 16px;
+    }
+</style>
