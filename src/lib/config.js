d73de379f18956f582d464539f4623d75df9300d
Gerrit Vermeulen <gerrit.vermeulen@assetpool.com>
2023-11-12 11:21:53 +0200

    First commit

---
+++ src/lib/config.js
@@ -0,0 +1,5 @@
+import { dev } from '$app/environment'
+
+export const title = 'Fire Lily Dance'
+export const description = 'Ignite your passion for dance'
+export const url = dev ? 'http://localhost:5173/' : 'https://firelily.dance'
