137b1b06f3df6b8d8696d5c740d8c8a9a7b74bf5
Gerrit Vermeulen <gertznezz@gmail.com>
2025-06-04 10:19:14 +0200

    Install node adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,13 @@ import { defineConfig } from 'astro/config';
 
 import svelte from '@astrojs/svelte';
 
+import node from '@astrojs/node';
+
 export default defineConfig({
   integrations: [svelte()],
   site: "https://firelily.dance",
+
+  adapter: node({
+    mode: 'standalone',
+  }),
 });
